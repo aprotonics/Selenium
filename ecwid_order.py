14d46969617438f299e2693339c86ead0cc77d5f
aprotonics <aprotonics@yandex.ru>
2022-02-08 17:28:34 +0500

    update

--- ecwid_order.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://app.ecwid.com/api/v3/59965088/orders?token=public_tgixALWxbCn2aymYLz2a2uAKXX5cSrCQ"
-
-headers = {"Accept": "application/json"}
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
