954246ae61d1a6f278e3aa8d7fd6d774855b4550
aprotonics <aprotonics@yandex.ru>
2022-02-11 23:16:58 +0500

    upd

--- test_fast_order.js
+++ test_fast_order.js
@@ -8,7 +8,9 @@ address_input_selector = '#ec-address-line1'
 city_input_selector = '#ec-city-list'
 postal_input_selector = '#ec-postal-code'
 place_order_button2_selector = '.form-control__button > .form-control__loader'
-thanks_for_order_block_selector = 'div.ec-store__confirmation-page h1.page-title__name'
+thanks_block_selector = 'div.ec-store__confirmation-page h1.page-title__name'
+
+timeInterval = 100
 
 
 async function loadScript(url) {
@@ -17,6 +19,14 @@ async function loadScript(url) {
   eval(script);
 }
 
+async function fillInput(input, value) {
+  input.value = value;
+  await input.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true }));
+  await input.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true }));
+  await input.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true }));
+  await input.dispatchEvent(new Event('input', { bubbles: true }));
+  await input.dispatchEvent(new Event('change', { bubbles: true }));
+}
 
 async function tovar_page() {
   to_cart_button = await document.querySelector(to_cart_button_selector)
@@ -25,7 +35,6 @@ async function tovar_page() {
   cart_icon = await document.querySelector(cart_icon_selector)
   await cart_icon.click()
 }
-
 
 function cart_page() {
   let email_input
@@ -39,93 +48,76 @@ function cart_page() {
   }
 
   async function fillCartPage() {
-    email_input.value = 'test@mail.ru';
-    await email_input.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true }));
-    await email_input.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true }));
-    await email_input.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true }));
-    await email_input.dispatchEvent(new Event('input', { bubbles: true }));
-    await email_input.dispatchEvent(new Event('change', { bubbles: true })); 
+    fillInput(email_input, 'test@mail.ru')
 
     checkbox_agree = await document.querySelector(checkbox_agree_selector)
     await checkbox_agree.click()
-
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve("готово!"), 1000)
-      });
-
-    let result = await promise
 
     place_order_button = await document.querySelector(place_order_button_selector)
     await place_order_button.click()
   }
     
-  let timerId = setInterval(findEmailInput, 400);  
+  let timerId = setInterval(findEmailInput, timeInterval);  
 }
 
-//Add awaiting name_input appearance
-async function checkout_page() {
-  name_input = await document.querySelector(name_input_selector)
-  name_input.value = 'test'
-  await name_input.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true }));
-  await name_input.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true }));
-  await name_input.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true }));
-  await name_input.dispatchEvent(new Event('input', { bubbles: true }));
-  await name_input.dispatchEvent(new Event('change', { bubbles: true })); 
 
-  address_input = await document.querySelector(address_input_selector)
-  address_input.value = 'test'
-  await address_input.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true }));
-  await address_input.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true }));
-  await address_input.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true }));
-  await address_input.dispatchEvent(new Event('input', { bubbles: true }));
-  await address_input.dispatchEvent(new Event('change', { bubbles: true })); 
+function checkout_page() {
+  let name_input
 
-  city_input = await document.querySelector(city_input_selector)
-  city_input.value = 'test'
-  await city_input.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true }));
-  await city_input.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true }));
-  await city_input.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true }));
-  await city_input.dispatchEvent(new Event('input', { bubbles: true }));
-  await city_input.dispatchEvent(new Event('change', { bubbles: true })); 
+  function findNameInput() {
+    name_input = document.querySelector(name_input_selector)
+    if (name_input) {
+      clearInterval(timerId)
+      fillCheckoutPage()
+    }
+  }
 
-  postal_input = await document.querySelector(postal_input_selector)
-  postal_input.value = '100000'
-  await postal_input.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true }));
-  await postal_input.dispatchEvent(new KeyboardEvent('keypress', { bubbles: true }));
-  await postal_input.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true }));
-  await postal_input.dispatchEvent(new Event('input', { bubbles: true }));
-  await postal_input.dispatchEvent(new Event('change', { bubbles: true })); 
+  async function fillCheckoutPage() { 
+    fillInput(name_input, 'test')
 
-  let promise = new Promise((resolve, reject) => {
-      setTimeout(() => resolve("готово!"), 1000)
-    });
+    address_input = await document.querySelector(address_input_selector)
+    fillInput(address_input, 'test')
 
-  let result = await promise
+    city_input = await document.querySelector(city_input_selector)
+    fillInput(city_input, 'test')
 
-  place_order_button2 = await document.querySelector(place_order_button2_selector)
-  await place_order_button2.click()
+    postal_input = await document.querySelector(postal_input_selector)
+    fillInput(postal_input, '100000')
+
+    place_order_button2 = await document.querySelector(place_order_button2_selector)
+    await place_order_button2.click()
+  }
+
+  let timerId = setInterval(findNameInput, timeInterval); 
 }
 
-//Add awaiting thanks_for_order_block appearance
-async function assert_page() {
-  thanks_for_order_block = await document.querySelector(thanks_for_order_block_selector)
-  thanks_for_order_text = thanks_for_order_block.textContent
+function assert_page(time) {
+  let thanks_block
 
-  let scriptUrl1 = 'https://cdnjs.cloudflare.com/ajax/libs/mocha/3.2.0/mocha.js'
-  let scriptUrl2 = 'https://cdnjs.cloudflare.com/ajax/libs/chai/3.5.0/chai.js'
-  await loadScript(scriptUrl1);
-  await loadScript(scriptUrl2);
+  function findThanksBlock(time) {
+    thanks_block = document.querySelector(thanks_block_selector)
+    if (thanks_block) {
+      clearInterval(timerId)
+      fillAssertPage(time)
+    }
+  }
+  
+  async function fillAssertPage(time) {
+    thanks_text = thanks_block.textContent
 
-  let assert = await chai.assert
-  await mocha.setup('bdd')
+    let scriptUrl = 'https://cdnjs.cloudflare.com/ajax/libs/chai/3.5.0/chai.js'
+    await loadScript(scriptUrl);
 
-  await assert.include(thanks_for_order_text, 'Спасибо за заказ')
+    await chai.assert.include(thanks_text, 'Спасибо за заказ')
 
-  await mocha.run()
+    await console.log("OK")
 
-  await console.log("OK")
+    time = performance.now() - time;
+    console.log('Время выполнения = ', time, ' мс');
+  }
+
+  let timerId = setInterval(findThanksBlock, timeInterval, time); 
 }
-
 
 async function run() {
   let time = performance.now();
@@ -134,29 +126,14 @@ async function run() {
 
   await cart_page()
 
-  let promise2 = new Promise((resolve, reject) => {
-      setTimeout(() => resolve("готово!"), 1000)
-    });
-
-  let result2 = await promise2
-
   await checkout_page()
 
-  let promise3 = new Promise((resolve, reject) => {
-      setTimeout(() => resolve("готово!"), 1500)
-    });
-
-  let result3 = await promise3
-
-  await assert_page()
-
-  time = performance.now() - time;
-  console.log('Время выполнения = ', time, ' мс');
+  await assert_page(time)
   }
 
 run()
 
-// Заменить setTimeout на асинхронность!!!
-
-
+// Стартовать с главной страницы!!!
+// Добавить проверку заказа через API https://api-docs.ecwid.com/reference/get-order
+// Исправить названия на CamelCase
 
